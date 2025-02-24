b9e8ed835418b15020008f8baf98f5f87b0dfdd2
YakubovaMakhliyo <122361044+YakubovaMakhliyo@users.noreply.github.com>
2025-02-24 15:35:47 +0500

    link

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
-import logos from "assets/logoadore.webp"
+// import logos from "assets/logoadore.webp"
 
 export default function Logo() {
   return (
@@ -10,13 +10,13 @@ export default function Logo() {
       href={"/"}
       className="hover:opacity-70 hover:text-[#D4AF37] text-3xl uppercase font-bold flex items-center pt-2"
     >
-      <Image
+      {/* <Image
         src={logos}
         width={60}
         height={60}
         className="object-cover"
         alt="Logo"
-      />
+      /> */}
       Adore
     </Link>
   );
--- services/axios.ts
+++ services/axios.ts
@@ -3,7 +3,7 @@ import { IToken } from "@/interface";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: "http://164.92.188.250/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
